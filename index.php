6745af7a104b5d5d3764616eba70bcb2647b4f3e
jinwoo lee <jinwoolee@jinwooui-MacBookPro.local>
2018-10-17 23:06:01 +0900

    add cookie

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 
 <?php
+	session_start();
+	$_SESSION['id'] = 'test';
 	$cdir = scandir('.');
 	foreach ($cdir as $key => $value){
 		if( substr($value, -4) === ".php"){
 			echo "<a href='{$value}'>{$value}</a><br/>";
 		}
 	}
-
+	
+	echo "<br/><hr/>";
+	highlight_file(__file__);	
